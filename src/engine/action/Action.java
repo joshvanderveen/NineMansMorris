f4272aba7e932c7471392f21dec1b8fe1cca853d
SomeGithubNoob <dubbajosh123@gmail.com>
2023-04-11 14:09:48 +1000

    Started building out sample engine

--- src/engine/action/Action.java
+++ src/engine/action/Action.java
@@ -2,9 +2,7 @@ package engine;
 
 public abstract class Action {
     public abstract String execute(Actor actor, Game game);
-
     public abstract String menuDescription(Actor actor);
-
     public String menuKey() {
         return null;
     }
---
+++ src/engine/action/ActionList.java
@@ -0,0 +1,2 @@
+package engine.action;public class ActionList {
+}
