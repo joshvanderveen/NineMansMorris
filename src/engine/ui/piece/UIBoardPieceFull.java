12daba475f3af6d5e4bc871121b6bd020d2caa3f
Rebekah <rful0003@student.monash.edu>
2023-04-25 18:36:13 +1000

    removed BoardMouseListener

--- src/engine/ui/piece/UIBoardPieceFull.java
+++ src/engine/ui/piece/UIBoardPieceFull.java
@@ -3,7 +3,7 @@ package engine.ui.piece;
 import engine.board.Piece;
 
 public class UIBoardPieceFull extends UIBoardPiece {
-    Piece piece;
+    private Piece piece;
     public UIBoardPieceFull(int x, int y, Piece piece) {
         super(x, y);
         this.piece = piece;
