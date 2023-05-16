af8f0973a331e4aafe0061dded2091a697815cca
jvan0048 <jvan0048@student.monash.edu>
2023-05-16 13:56:11 +1000

    Person description panels added

--- src/engine/ui/UIMainGui.java
+++ src/engine/ui/UIMainGui.java
@@ -15,11 +15,15 @@ public class UIMainGui extends JFrame {
     private UIBoardPanel UIBoardPanel;
     private JPanel buttons;
 
+    private JPanel playerInfoContainer;
+    private ArrayList<UIPlayerDescriptionPanel> descriptionPanels;
+
+
     public UIMainGui() {
         super("Nine Man's Morris");
 
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(800, 800);
+        this.setSize(1000, 800);
 
         try {
             UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
@@ -71,48 +75,29 @@ public class UIMainGui extends JFrame {
         buttons.add(controlsButton);
         buttons.add(exitButton);
 
-        if (UIBoardPanel.getGameBoard() != null) {
-            JPanel playerInfo = new JPanel();
-            BoxLayout playerInfoBox = new BoxLayout(playerInfo, BoxLayout.Y_AXIS);
-            playerInfo.setLayout(playerInfoBox);
+        buttons.setBackground(Color.WHITE);
 
-            ArrayList<HashMap<String, Integer>> playerPieceInfo;
-            playerPieceInfo = UIBoardPanel.getGameBoard().getPlayerPieceInfo();
-
-            JTextArea player1Info = new JTextArea(
-                    String.format("Player 1" +
-                            "\nPieces in play:   %d" +
-                            "\nPieces lost:      %d" +
-                            "\nPieces to place:  %d", playerPieceInfo.get(0).get("placed"), playerPieceInfo.get(0).get("removed"), playerPieceInfo.get(0).get("unplaced"))
-            );
-            player1Info.setEditable(false);
-            JTextArea player2Info = new JTextArea(
-                    String.format("Player 2" +
-                            "\nPieces in play:  %d" +
-                            "\nPieces lost:     %d" +
-                            "\nPieces to place: %d", playerPieceInfo.get(1).get("placed"), playerPieceInfo.get(1).get("removed"), playerPieceInfo.get(1).get("unplaced"))
-            );
-            player2Info.setEditable(false);
-
-            playerInfo.add(player1Info);
-            playerInfo.add(player2Info);
-
-            this.add(playerInfo, BorderLayout.EAST);
-        }
+        playerInfoContainer = new JPanel();
+        BoxLayout playerInfoBox = new BoxLayout(playerInfoContainer, BoxLayout.Y_AXIS);
+        playerInfoContainer.setLayout(playerInfoBox);
 
         this.add(UIBoardPanel, BorderLayout.CENTER);
         this.add(buttons, BorderLayout.NORTH);
 
-        // TODO: Implement properly
-//        PlayerDescription player1 = new PlayerDescription(new HumanPlayer());
-//        PlayerDescription player2 = new PlayerDescription(new ComputerPlayer());
-
-        this.setVisible(true);
     }
 
     public void setGameBoard(GameBoard gameBoard) {
         UIBoardPanel.setGameBoard(gameBoard);
         redraw();
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        for (Player player : players) {
+            UIPlayerDescriptionPanel playerDescription = new UIPlayerDescriptionPanel(player);
+            playerInfoContainer.add(playerDescription);
+        }
+        playerInfoContainer.setBackground(Color.WHITE);
+        this.add(playerInfoContainer, BorderLayout.EAST);
     }
 
     public void redraw() {
--- src/engine/ui/UIPlayerDescriptionPanel.java
+++ src/engine/ui/UIPlayerDescriptionPanel.java
@@ -11,6 +11,7 @@ public class UIPlayerDescriptionPanel extends JPanel {
     private JLabel playerName;
     private JLabel playerColor;
     private JLabel piecesToPlace;
+    private JLabel piecesInPlay;
     private JLabel piecesTaken;
 
     public UIPlayerDescriptionPanel(Player player) {
@@ -18,18 +19,29 @@ public class UIPlayerDescriptionPanel extends JPanel {
 
         this.player = player;
 
-        this.setLayout(new FlowLayout());
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 
         playerName = new JLabel("Name: " + this.player.getName());
         playerColor = new JLabel("Colour: " + this.player.getPlayerColor().toString());
         // TODO: Fix implementation to show amount of pieces player has to place
         piecesToPlace = new JLabel("Pieces to place: " + 0);
+        piecesInPlay = new JLabel("Pieces in play: " + 0);
         // TODO: Fix implementation to show amount of pieces player has had taken
         piecesTaken = new JLabel("Pieces to place: " + 0);
 
         this.add(playerName);
         this.add(playerColor);
         this.add(piecesToPlace);
+        this.add(piecesInPlay);
         this.add(piecesTaken);
+
+        playerName.setFont(new Font("Arial", Font.PLAIN, 18));
+        playerColor.setFont(new Font("Arial", Font.PLAIN, 14));
+        piecesToPlace.setFont(new Font("Arial", Font.PLAIN, 14));
+        piecesInPlay.setFont(new Font("Arial", Font.PLAIN, 14));
+        piecesTaken.setFont(new Font("Arial", Font.PLAIN, 14));
+
+        this.setBackground(Color.WHITE);
+        this.setBorder(BorderFactory.createEmptyBorder(0, 0, 30, 20));
     }
 }
